1d735526f7e8fca7c979c886f32edcc264013b26
cre8rec <juliantmayes@gmail.com>
2020-04-30 13:10:34 -0700

    rockpaperscissors

---
+++ script.js
@@ -0,0 +1,192 @@
+var game, rounds, playerMove, sum, gamePlaying, finalResult;
+
+var playerScore = 0 
+var compScore = 0 
+
+sum = 0;
+
+var game = 0
+
+var newGameButton = document.querySelector('.newGame')
+
+//computer random 
+function compMove(){
+    return computerSelection = Math.floor(Math.random() * 3);
+};
+
+
+
+
+/*function score(sum){
+    result()
+    if (sum = 3, 4, 9){playerScore++}
+    else{playerScore += 0}
+    return playerScore
+};*/
+
+
+
+function winner() { 
+    let message = '';
+    if (sum == 3){message += 'you win with rock'}
+    else if (sum == 4){message +=  'you win with paper'}
+        else if (sum == 9){message += 'you win with scissors'}
+           else if (sum == 2){message +=  'computer wins with paper'}
+               else if (sum == 6){message +=  'computer wins with scissors'}
+                   else if (sum == 8){message +=  'computer wins with rock'}
+    else if (sum !== 3, 4, 9, 2, 6, 8) {message += 'its a draw'};
+    return message   
+}
+
+//
+function result(playerMove){
+    sum = compMove() + playerMove;
+   // return winner()
+  
+    return winner() + ' ' 
+};
+    
+function updatePlayerScore(answer){
+    if (answer.includes('you')){playerScore++}
+    return playerScore
+}
+
+function updateCompScore(answer){
+    if (answer.includes('computer')){compScore++}
+    return compScore
+}
+    
+var finalResult = document.querySelector('.gameWinner');
+
+
+function checkResult(playerScore, compScore) {
+    if(playerScore > compScore){
+        
+        finalResult.textContent = "You Won!";
+        newGame()
+        
+        
+    } else if(playerScore < compScore) {
+        
+        finalResult.textContent = "You Lost";
+        newGame()
+        
+    }  
+}
+
+
+
+function newGame(){
+        console.log('new game?');
+        newGameButton.style.display = 'flex'
+       }
+
+
+
+
+const rock = document.querySelector('.rock');
+rock.onclick = () => {
+
+    roundWinner = document.querySelector('.roundWinner').textContent = result(1);
+    
+    let playerScore = document.querySelector('.my-score').textContent = updatePlayerScore(roundWinner);
+    let compScore = document.querySelector('.comp-score').textContent = updateCompScore(roundWinner);
+    
+
+    if (playerScore === 5 || compScore === 5){checkResult(playerScore, compScore)}
+
+    newGameButton.onclick = () => {console.log('hello')
+                              init();}
+  
+                     }
+
+const paper = document.querySelector('.paper');
+paper.onclick = () => {
+    
+    roundWinner = document.querySelector('.roundWinner').textContent = result(4);
+    
+    playerScore = document.querySelector('.my-score').textContent = updatePlayerScore(roundWinner);
+    compScore = document.querySelector('.comp-score').textContent = updateCompScore(roundWinner);
+    
+    
+    if (playerScore === 5 || compScore === 5){checkResult(playerScore, compScore)}
+    
+    newGameButton.onclick = () => {console.log('hello')
+                              init();}
+                      }
+
+const scissors = document.querySelector('.scissors');
+scissors.onclick = () => {
+    roundWinner = document.querySelector('.roundWinner').textContent = result(8);
+    
+    playerScore = document.querySelector('.my-score').textContent = updatePlayerScore(roundWinner);
+    compScore = document.querySelector('.comp-score').textContent = updateCompScore(roundWinner);
+     
+    
+    if (playerScore === 5 || compScore === 5){checkResult(playerScore, compScore)}
+
+    
+    newGameButton.onclick = () => {console.log('hello')
+                              init();}
+                         
+                         }
+
+
+
+function init(){
+    playerScore = 0
+    compScore = 0
+    document.querySelector('.my-score').textContent = 0
+    document.querySelector('.comp-score').textContent = 0
+
+    this.finalResult.textContent = ' '
+    document.querySelector('.roundWinner').textContent = ' '
+    newGameButton.style.display = 'none'
+    
+}
+
+
+
+
+
+
+/*function playRound(playerSelection) {
+    let game = 0
+    let computerSelection = Math.floor(Math.random() * 3);
+   
+    let playerMove = playerSelection.toLocaleLowerCase();
+        if (playerMove == 'rock'){game += 1}
+            else if (playerMove == 'paper'){game += 4}
+                else {game += 8};
+    
+
+
+    
+    let sum = computerSelection + game;
+    var message = '' 
+        
+    let computerDisplay = ''
+   
+           if (computerSelection == 0){computerDisplay += 'computer chooses rock, '}
+                else if (computerSelection == 1){computerDisplay += 'computer chooses paper, '}
+                    else {computerDisplay += 'computer chooses scissors, '};
+            
+    
+    
+if (sum == 3){message += 'you win with rock'}
+    else if (sum == 4){message +=  'you win with paper'}
+        else if (sum == 9){message += 'you win with scissors'}
+           else if (sum == 2){message +=  'computer wins with paper'}
+               else if (sum == 6){message +=  'computer wins with scissors'}
+                   else if (sum == 8){message +=  'computer wins with rock'}
+    else if (sum !== 3, 4, 9, 2, 6, 8) { message += 'its a draw'};
+                        
+var winner = document.write(computerDisplay,message); 
+return winner;*/
+
+
+
+
+
+
+
